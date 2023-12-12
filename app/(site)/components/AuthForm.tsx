14f89b6da1941031acbf897868bcae19dd381200
AH-Likhon <ictianlikhon6@gmail.com>
2023-12-12 18:14:03 +0600

    Loggedin using credentials implemented

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,8 @@ import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import AuthSocialBtn from "./AuthSocialBtn";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -39,11 +41,26 @@ const AuthForm = () => {
 
     if (variant === "REGISTER") {
       // Call axios for register
-      axios.post("api/register", data);
+      axios
+        .post("api/register", data)
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false));
     }
 
     if (variant === "LOGIN") {
       // NextAuth Signin
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid Credentials");
+          } else if (callback?.ok) {
+            toast.success("Logged In!");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
   };
 
--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -7,6 +7,9 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { email, name, password } = body;
 
+  if (!email || !name || !password) {
+    return new NextResponse("Missing Info!", { status: 400 });
+  }
   const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
