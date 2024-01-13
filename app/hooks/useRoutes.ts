c204356f408f052b6c571413b550a6ab924c550e
AH-Likhon <ictianlikhon6@gmail.com>
2024-01-13 22:20:09 +0600

    [DONE] desktop sidebar and mobile footer

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -1,2 +1,39 @@
 import { useMemo } from "react";
-import { useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { HiChat } from "react-icons/hi";
+import { HiUsers, HiArrowLeftOnRectangle } from "react-icons/hi2";
+import useConversation from "./useConversation";
+import { signOut } from "next-auth/react";
+
+const useRoutes = () => {
+  const pathName = usePathname();
+  const { conversationId } = useConversation();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Chat",
+        href: "/conversations",
+        icon: HiChat,
+        active: pathName === "/conversations" || !!conversationId,
+      },
+      {
+        label: "Users",
+        href: "/users",
+        icon: HiUsers,
+        active: pathName === "/users",
+      },
+      {
+        label: "Logout",
+        href: "#",
+        onClick: () => signOut(),
+        icon: HiArrowLeftOnRectangle,
+      },
+    ],
+    [pathName, conversationId]
+  );
+
+  return routes;
+};
+
+export default useRoutes;
